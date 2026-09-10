e756ddacbe3f9674ce7a4d7c4afded8e7c62b742
earnest-s <s.earnestofficial@gmail.com>
2026-09-10 15:14:05 +0530

    Improvements

--- LoqNova.Avalonia/ViewModels/Pages/DashboardViewModel.cs
+++ LoqNova.Avalonia/ViewModels/Pages/DashboardViewModel.cs
@@ -1,11 +1,19 @@
 using System;
-using System.Collections.ObjectModel;
+using System.Collections.ObjectCollection;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using LoqNova.Avalonia.Services;
 
 namespace LoqNova.Avalonia.ViewModels.Pages;
+
+public enum PowerModeState
+{
+    Quiet,
+    Balance,
+    Performance,
+    GodMode
+}
 
 public partial class DashboardViewModel : ViewModelBase
 {
