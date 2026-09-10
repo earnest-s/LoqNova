0b392ee68f359da224041152088bb58af2000cde
earnest-s <s.earnestofficial@gmail.com>
2026-09-10 18:18:22 +0530

    Improvements

--- LoqNova.Avalonia/Services/ITrayService.cs
+++ LoqNova.Avalonia/Services/ITrayService.cs
@@ -1,11 +1,12 @@
 using System.Threading.Tasks;
+using Avalonia.Controls;
 using LoqNova.Avalonia.Services;
 
 namespace LoqNova.Avalonia.Services;
 
 public interface ITrayService
 {
-    Task InitializeAsync(Avalonia.Controls.Window mainWindow);
+    Task InitializeAsync(Window mainWindow);
     Task UpdateTooltipAsync(string tooltip);
     Task ShutdownAsync();
 }
