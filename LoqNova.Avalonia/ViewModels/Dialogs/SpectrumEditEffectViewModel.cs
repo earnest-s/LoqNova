2dec76dc748083ee56b1530c8d9d03404a0c69c4
earnest-s <s.earnestofficial@gmail.com>
2026-09-10 18:13:02 +0530

    Improvements

--- LoqNova.Avalonia/ViewModels/Dialogs/SpectrumEditEffectViewModel.cs
+++ LoqNova.Avalonia/ViewModels/Dialogs/SpectrumEditEffectViewModel.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.ObjectCollection;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
