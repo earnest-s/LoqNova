f1d61d9df19230fcc02e084bd07aabc03740b7a0
earnest-s <s.earnestofficial@gmail.com>
2026-09-10 09:51:47 +0530

    Improvements

---
+++ LoqNova.Avalonia/Services/IMacroService.cs
@@ -0,0 +1,48 @@
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace LoqNova.Avalonia.Services;
+
+public class MacroKey
+{
+    public int KeyNumber { get; set; }
+    public string Name { get; set; } = "";
+    public ObservableCollection<MacroEvent> Events { get; } = new();
+    public bool Enabled { get; set; }
+}
+
+public abstract class MacroEvent
+{
+    public double DelayMs { get; set; }
+}
+
+public class MacroKeyEvent : MacroEvent
+{
+    public string Key { get; set; } = "";
+    public bool IsPress { get; set; } = true;
+}
+
+public class MacroMouseEvent : MacroEvent
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public string Button { get; set; } = "Left";
+    public bool IsPress { get; set; } = true;
+}
+
+public interface IMacroService
+{
+    bool IsEnabled { get; set; }
+    ObservableCollection<MacroKey> MacroKeys { get; }
+    int SelectedKeyNumber { get; set; }
+    bool IsRecording { get; }
+    
+    event Action<MacroKey> MacroKeyChanged;
+    event Action<bool> RecordingStateChanged;
+    
+    Task InitializeAsync();
+    Task StartRecordingAsync(int keyNumber);
+    Task StopRecordingAsync();
+    Task PlayMacroAsync(int keyNumber);
+    Task SaveAsync();
+}
