121fb253348eeafe1ac628570e2f7d0b7f12ff0f
earnest-s <s.earnestofficial@gmail.com>
2026-09-10 09:50:05 +0530

    Improvements

---
+++ LoqNova.Avalonia/Services/IBatteryService.cs
@@ -0,0 +1,50 @@
+using System.Threading.Tasks;
+
+namespace LoqNova.Avalonia.Services;
+
+public enum BatteryState
+{
+    Normal,
+    RapidCharge,
+    Conservation
+}
+
+public enum BatteryNightChargeState
+{
+    Disabled,
+    Enabled
+}
+
+public interface IBatteryService
+{
+    int Percentage { get; }
+    string StatusText { get; }
+    bool IsCharging { get; }
+    bool IsLowBattery { get; }
+    bool IsLowWattageCharger { get; }
+    double TemperatureC { get; }
+    double TemperatureF { get; }
+    double DischargeRate { get; }
+    double MinDischargeRate { get; }
+    double MaxDischargeRate { get; }
+    int CurrentCapacity { get; }
+    int FullChargeCapacity { get; }
+    int DesignCapacity { get; }
+    int HealthPercent { get; }
+    TimeSpan OnBatteryDuration { get; }
+    DateTime? OnBatterySince { get; }
+    int CycleCount { get; }
+    DateTime? ManufactureDate { get; }
+    DateTime? FirstUseDate { get; }
+    BatteryState CurrentMode { get; }
+    BatteryNightChargeState NightChargeMode { get; }
+    bool IsPowerAdapterConnected { get; }
+    
+    event Action<int> PercentageChanged;
+    event Action<bool> ChargingChanged;
+    event Action<BatteryState> ModeChanged;
+    
+    Task InitializeAsync();
+    Task SetModeAsync(BatteryState mode);
+    Task SetNightChargeAsync(BatteryNightChargeState state);
+}
