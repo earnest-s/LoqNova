64fef6ed21dd0f3b2827de63ec4a06b54abe4fe7
earnest-s <s.earnestofficial@gmail.com>
2026-09-10 10:03:00 +0530

    Improvements

---
+++ LoqNova.Avalonia/Services/ISensorsService.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+
+namespace LoqNova.Avalonia.Services;
+
+public interface ISensorsService
+{
+    double CpuUsage { get; }
+    double GpuUsage { get; }
+    double CpuTemperature { get; }
+    double GpuTemperature { get; }
+    int FanSpeedRpm { get; }
+    
+    event Action<double> CpuUsageChanged;
+    event Action<double> GpuUsageChanged;
+    event Action<double> CpuTemperatureChanged;
+    event Action<double> GpuTemperatureChanged;
+    event Action<int> FanSpeedChanged;
+    
+    Task InitializeAsync();
+}
