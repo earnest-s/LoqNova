bb5c7b72d94052a79d6c8f7af804dfa2dc44d95a
earnest-s <s.earnestofficial@gmail.com>
2026-09-10 09:52:38 +0530

    Improvements

---
+++ LoqNova.Avalonia/Services/MockPerformanceService.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Threading.Tasks;
+using LoqNova.Lib.Features.PowerMode;
+using LoqNova.Avalonia.Services;
+
+namespace LoqNova.Avalonia.Services;
+
+public class MockPerformanceService : IPerformanceService
+{
+    public PowerModeState CurrentMode { get; private set; } = PowerModeState.Balance;
+    public bool IsSupported { get; } = true;
+    public bool IsGodModeEnabled { get; private set; } = false;
+    
+    private double _cpuPowerLimit = 45;
+    private double _gpuPowerLimit = 80;
+    private double _cpuThermalLimit = 85;
+    private double _gpuThermalLimit = 83;
+    
+    public double CpuPowerLimit
+    {
+        get => _cpuPowerLimit;
+        set { _cpuPowerLimit = value; CpuPowerLimitChanged?.Invoke(value); }
+    }
+    
+    public double GpuPowerLimit
+    {
+        get => _gpuPowerLimit;
+        set { _gpuPowerLimit = value; GpuPowerLimitChanged?.Invoke(value); }
+    }
+    
+    public double CpuThermalLimit
+    {
+        get => _cpuThermalLimit;
+        set { _cpuThermalLimit = value; }
+    }
+    
+    public double GpuThermalLimit
+    {
+        get => _gpuThermalLimit;
+        set { _gpuThermalLimit = value; }
+    }
+    
+    public event Action<PowerModeState>? ModeChanged;
+    public event Action<double>? CpuPowerLimitChanged;
+    public event Action<double>? GpuPowerLimitChanged;
+    
+    public Task InitializeAsync()
+    {
+        // Simulate some initial state
+        CurrentMode = PowerModeState.Balance;
+        return Task.CompletedTask;
+    }
+    
+    public Task SetModeAsync(PowerModeState mode)
+    {
+        CurrentMode = mode;
+        ModeChanged?.Invoke(mode);
+        return Task.CompletedTask;
+    }
+    
+    public Task ApplyGodModeSettingsAsync()
+    {
+        IsGodModeEnabled = !IsGodModeEnabled;
+        return Task.CompletedTask;
+    }
+}
