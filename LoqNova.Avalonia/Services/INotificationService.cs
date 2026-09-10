2f1a9a934a38319b8c95ba74b67287e48803acff
earnest-s <s.earnestofficial@gmail.com>
2026-09-10 18:17:46 +0530

    Improvements

--- LoqNova.Avalonia/Services/INotificationService.cs
+++ LoqNova.Avalonia/Services/INotificationService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 
 namespace LoqNova.Avalonia.Services;
@@ -42,7 +43,7 @@ public class NotificationMessage
 
 public interface INotificationService
 {
-    event Action<NotificationMessage> NotificationRequested;
+    event Action<NotificationMessage>? NotificationRequested;
     
     Task InitializeAsync();
     Task ShowAsync(NotificationMessage message);
