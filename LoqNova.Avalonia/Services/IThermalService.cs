7cd6757661a678e4f708028d5de6e8a03a9b5c74
earnest-s <s.earnestofficial@gmail.com>
2026-09-10 09:49:22 +0530

    Improvements

---
+++ LoqNova.Avalonia/Services/IThermalService.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+
+namespace LoqNova.Avalonia.Services;
+
+public interface IThermalService
+{
+    double CpuTemperature { get; }
+    double GpuTemperature { get; }
+    int FanSpeedRpm { get; }
+    int FanSpeedPercent { get; }
+    bool IsFanControlSupported { get; }
+    
+    event Action<double> CpuTemperatureChanged;
+    event Action<double> GpuTemperatureChanged;
+    event Action<int> FanSpeedChanged;
+    
+    Task InitializeAsync();
+    Task SetFanSpeedAsync(int percent);
+    Task SetFanCurveAsync(FanCurvePoint[] curve);
+}
+
+public struct FanCurvePoint
+{
+    public double Temperature;
+    public int FanSpeedPercent;
+    
+    public FanCurvePoint(double temperature, int fanSpeedPercent)
+    {
+        Temperature = temperature;
+        FanSpeedPercent = fanSpeedPercent;
+    }
+}
