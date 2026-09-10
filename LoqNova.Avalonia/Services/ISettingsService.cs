32d0b393ada236a41838e7263b6f49f114bc9873
earnest-s <s.earnestofficial@gmail.com>
2026-09-10 09:51:00 +0530

    Improvements

---
+++ LoqNova.Avalonia/Services/ISettingsService.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using LoqNova.Avalonia.Services;
+
+namespace LoqNova.Avalonia.Services;
+
+public interface ISettingsService
+{
+    AppTheme Theme { get; set; }
+    string AccentColor { get; set; }
+    bool UseSystemAccent { get; set; }
+    string Language { get; set; }
+    bool TemperatureUnitFahrenheit { get; set; }
+    bool AutorunEnabled { get; set; }
+    bool MinimizeToTray { get; set; }
+    bool MinimizeOnClose { get; set; }
+    bool VantageDisabled { get; set; }
+    bool LegionZoneDisabled { get; set; }
+    bool FnKeysDisabled { get; set; }
+    bool SmartFnLockEnabled { get; set; }
+    int SmartFnLockModifierKey { get; set; }
+    string SmartKeySinglePressAction { get; set; }
+    string SmartKeyDoublePressAction { get; set; }
+    bool GodModeFnQSwitchable { get; set; }
+    bool NotificationsEnabled { get; set; }
+    bool CliEnabled { get; set; }
+    bool HwInfoEnabled { get; set; }
+    string HwInfoSharedMemoryPath { get; set; }
+    bool SyncBrightnessToAllPowerPlans { get; set; }
+    bool ResetBatteryOnSinceOnReboot { get; set; }
+    
+    event Action SettingsChanged;
+    
+    Task InitializeAsync();
+    Task SaveAsync();
+}
