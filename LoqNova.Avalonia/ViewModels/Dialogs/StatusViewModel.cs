12b5717e86ace2d25ebc5ffea06309c982946f2d
earnest-s <s.earnestofficial@gmail.com>
2026-09-10 10:18:15 +0530

    Improvements

---
+++ LoqNova.Avalonia/ViewModels/Dialogs/StatusViewModel.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.ObjectCollection;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace LoqNova.Avalonia.ViewModels.Dialogs;
+
+public partial class StatusViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private string _batteryStatus = "82% - On battery";
+    
+    [ObservableProperty]
+    private string _powerModeStatus = "Balance";
+    
+    [ObservableProperty]
+    private string _cpuTempStatus = "54°C";
+    
+    [ObservableProperty]
+    private string _gpuTempStatus = "49°C";
+    
+    [ObservableProperty]
+    private string _fanStatus = "2400 RPM (45%)";
+    
+    [ObservableProperty]
+    private string _rgbStatus = "Preset 1 - Static";
+    
+    [ObservableProperty]
+    private bool _isCharging = false;
+    
+    public StatusViewModel()
+    {
+    }
+    
+    [RelayCommand]
+    private async Task CloseAsync()
+    {
+    }
+}
---
+++ LoqNova.Avalonia/ViewModels/Dialogs/UnsupportedViewModel.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.ObjectCollection;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace LoqNova.Avalonia.ViewModels.Dialogs;
+
+public partial class UnsupportedViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private string _message = "This feature is not supported on your device.";
+    
+    [ObservableProperty]
+    private string _details = "";
+    
+    public UnsupportedViewModel()
+    {
+    }
+    
+    [RelayCommand]
+    private async Task CloseAsync()
+    {
+    }
+}
