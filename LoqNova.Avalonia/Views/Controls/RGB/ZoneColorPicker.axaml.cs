83ab68d488cdc410e92e0091df3270d50866af72
earnest-s <s.earnestofficial@gmail.com>
2026-09-25 22:20:55 +0530

    Improvements

--- LoqNova.Avalonia/Views/Controls/RGB/ZoneColorPicker.axaml.cs
+++ LoqNova.Avalonia/Views/Controls/RGB/ZoneColorPicker.axaml.cs
@@ -1,10 +1,30 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using LoqNova.Avalonia.Services;
 
 namespace LoqNova.Avalonia.Views.Controls.RGB;
 
 public partial class ZoneColorPicker : UserControl
 {
+    public static readonly StyledProperty<int> ZoneNumberProperty =
+        AvaloniaProperty.Register<ZoneColorPicker, int>(nameof(ZoneNumber));
+
+    public static readonly StyledProperty<RgbZoneColor> ColorProperty =
+        AvaloniaProperty.Register<ZoneColorPicker, RgbZoneColor>(nameof(Color), defaultBindingMode: Avalonia.Data.BindingMode.TwoWay);
+
+    public int ZoneNumber
+    {
+        get => GetValue(ZoneNumberProperty);
+        set => SetValue(ZoneNumberProperty, value);
+    }
+
+    public RgbZoneColor Color
+    {
+        get => GetValue(ColorProperty);
+        set => SetValue(ColorProperty, value);
+    }
+
     public ZoneColorPicker()
     {
         InitializeComponent();
