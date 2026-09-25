439d560664bbd052a3a4dceea612ef6c58b87c05
earnest-s <s.earnestofficial@gmail.com>
2026-09-25 21:32:38 +0530

    Improvements

--- LoqNova.Avalonia/Converters/BoolToVisibilityConverter.cs
+++ LoqNova.Avalonia/Converters/BoolToVisibilityConverter.cs
@@ -1,6 +1,6 @@
 using System;
 using Avalonia.Data.Converters;
-using Avalonia.Controls;
+using global::Avalonia.Controls;
 
 namespace LoqNova.Avalonia.Converters;
 
@@ -11,9 +11,9 @@ public class BoolToVisibilityConverter : IValueConverter
         if (value is bool boolValue)
         {
             var inverted = parameter is string str && str.Equals("invert", StringComparison.OrdinalIgnoreCase);
-            return (boolValue ^ inverted) ? Avalonia.Controls.Visibility.Visible : Avalonia.Controls.Visibility.Collapsed;
+            return (boolValue ^ inverted) ? global::Avalonia.Controls.Visibility.Visible : global::Avalonia.Controls.Visibility.Collapsed;
         }
-        return Avalonia.Controls.Visibility.Collapsed;
+        return global::Avalonia.Controls.Visibility.Collapsed;
     }
     
     public object? ConvertBack(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
